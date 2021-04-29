a2f3f38f657ed3cb3f7d5c02a59657d2583f981c
nihal <nihal.tca1609071@tmu.ac.in>
2021-04-30 02:49:56 +0530

    First change

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class  HomePage extends StatelessWidget {
         title: Text("Awsome App"),
       ),
       body: Container(
-        child: Center(child: Text("Hi Flutter")),
+        child: Center(child: Text("Hi Fulutter")),
       ),
     );
     
