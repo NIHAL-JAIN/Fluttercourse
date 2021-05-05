56d6d9b9978c9da6704c6ce77e525a61e37173f7
nihal <nihal.tca1609071@tmu.ac.in>
2021-05-05 15:46:48 +0530

    chp5 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
 
 
 void main() {
   runApp(
     MaterialApp(
       title: "Awsome App",
-      home: HomePage(),
+      home: LoginPage(),
       theme: ThemeData(
         primarySwatch: Colors.purple,
        ),
+       routes: {
+    "/login":(context)=> LoginPage(),
+    "/home" : (context)=> HomePage()
+  },
   ),
+  
   );
 }
 
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/bg_image.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login page"),
+        ),
+        body: Stack(
+          fit: StackFit.expand,
+          children:<Widget> [
+            BgImage(),
+            Center(
+              child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+              child: Card(
+                            child: Column (
+                  children: <Widget>[
+                    Form(
+                     child: Padding(
+                       padding: const EdgeInsets.all(16.0),
+                       child: Column(children: <Widget> [
+                         TextFormField(
+                           decoration: InputDecoration(
+                             hintText: "Enter Username",
+                             labelText: "Username") ,
+                         ),
+                         SizedBox(
+                           height:20,
+                           ),
+                          TextFormField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: "Enter Password",
+                              labelText: "Password") ,
+                         )
+                       ],
+                       ),
+                     )) ,
+                      SizedBox(
+                           height:20,
+                           ),  
+                           // ignore: deprecated_member_use
+                           Padding(
+                             padding: const EdgeInsets.all(8.0),
+                             // ignore: deprecated_member_use
+                             child: RaisedButton(
+                               onPressed: (){
+                                //  Navigator.push(
+                                //    context,
+                                //    MaterialPageRoute(
+                                //    builder: (context)=>HomePage()));
+                                Navigator.pushNamed(context, "/home");
+                               },
+                               child: Text("Sign In"),
+                               color: Colors.orange,
+                               textColor: Colors.white,
+                             ),
+                           ) 
+                  ],
+                ),
+              ),
+              ),
+        ),
+            ),
+          ],
+        )
+    );
+  }
+}
